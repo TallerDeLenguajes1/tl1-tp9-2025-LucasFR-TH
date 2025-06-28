e62f35a1cdea2520e181329dd4d52142a7a40112
lucas <lucas.f.roldan@gmail.com>
2025-06-28 10:05:40 -0300

    se termina el ejercicio 2

--- LectorTagMP3/Program.cs
+++ LectorTagMP3/Program.cs
@@ -15,6 +15,7 @@ string RutaArchivo = @"F:\PROGRAMACION\TALLER LENGUAJE\tp9\tl1-tp9-2025-LucasFR-
 
 if (File.Exists(RutaArchivo))
 {
+    // El programa deberá leer esta información
     FileStream MiArchivo = new FileStream("vacio.mp3", FileMode.Open);
 
     MiArchivo.Seek(-128, SeekOrigin.End);
@@ -22,9 +23,28 @@ if (File.Exists(RutaArchivo))
     MiArchivo.Read(buffer, 0, 128);
 
     string header = Encoding.GetEncoding("latin1").GetString(buffer, 0, 3);
-    if (header != "TAG") {
+    // CONTROL PARA SABER QUE HAY TAG VALIDO
+    if (header != "TAG")
+    {
         Console.WriteLine("NO HAY TAG");
-    } else {
-        Console.WriteLine("HAY TAG");
     }
+
+    // cargar en una instancia de una clase Id3v1Tag
+    Id3v1Tag tag = new Id3v1Tag
+    {
+        Titulo = Encoding.GetEncoding("latin1").GetString(buffer, 3, 30).TrimEnd('\0', ' '),
+        Artista = Encoding.GetEncoding("latin1").GetString(buffer, 33, 30).TrimEnd('\0', ' '),
+        Album = Encoding.GetEncoding("latin1").GetString(buffer, 63, 30).TrimEnd('\0', ' '),
+        Año = Encoding.GetEncoding("latin1").GetString(buffer, 93, 4).TrimEnd('\0', ' ')
+    };
+    
+    // y luego mostrar por consola el título, artista, álbum y año de la canción.
+    Console.WriteLine("Información del Tag ID3v1:");
+    Console.WriteLine($"Título: {tag.Titulo}");
+    Console.WriteLine($"Artista: {tag.Artista}");
+    Console.WriteLine($"Álbum: {tag.Album}");
+    Console.WriteLine($"Año: {tag.Año}");
+
 }
+    
+
