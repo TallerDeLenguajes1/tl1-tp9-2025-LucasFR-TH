a23736714e2f27b76176fc68e37ccf0c981ccc58
lucas <lucas.f.roldan@gmail.com>
2025-06-28 10:01:45 -0300

    completamos el ejercico 2 con la lectura correcta del archivo

--- LectorTagMP3/Program.cs
+++ LectorTagMP3/Program.cs
@@ -1,23 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
 using System.IO;
 using System.Text;
+using EspacioId3V1Tag;
 
+// Dentro de su repositorio cree una carpeta que se llame “LectorTagMP3” en ella crear una 
+// aplicación de consola en C# para leer el tag de un archivo MP3.
 Console.WriteLine("--------------------------------------");
 Console.WriteLine(".............ANALIZAR MP3.............");
 Console.WriteLine("--------------------------------------");
 
-Console.WriteLine("Ingrese el tag a analizar:");
-Stream fs = new Stream();
-
-if ()
-{
-    // verfiicar que los primeros 3 bits tienen TAG
-    // verificar la cantidad totla de bytes del arhcivoy le resto 128 para poderusar el seek
-    fs.Seek(-128, SeekOrigin.End);
-}
-
 byte[] buffer = new byte[128];
+string RutaArchivo = @"F:\PROGRAMACION\TALLER LENGUAJE\tp9\tl1-tp9-2025-LucasFR-TH\LectorTagMP3\vacio.mp3";
 
-int bytesleidos = fs.Read(buffer, 0, 128);
+if (File.Exists(RutaArchivo))
+{
+    FileStream MiArchivo = new FileStream("vacio.mp3", FileMode.Open);
 
-Encoding.Latin1.GetString(buffer, 0, 3);
+    MiArchivo.Seek(-128, SeekOrigin.End);
+
+    MiArchivo.Read(buffer, 0, 128);
+
+    string header = Encoding.GetEncoding("latin1").GetString(buffer, 0, 3);
+    if (header != "TAG") {
+        Console.WriteLine("NO HAY TAG");
+    } else {
+        Console.WriteLine("HAY TAG");
+    }
+}
---
+++ LectorTagMP3/iD3V1tag.cs
@@ -0,0 +1,8 @@
+namespace EspacioId3V1Tag;
+public class Id3v1Tag
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public string Album { get; set; }
+    public string Año { get; set; }
+}
